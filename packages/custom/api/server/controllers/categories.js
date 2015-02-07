a1c5d0ca3a120e5229ff17351eac425ea74ad8b0
Saleh Saiid <saleh.saiid@gmail.com>
2015-02-07 12:27:43 +0200

    [API] adds creation method to category controller

--- packages/custom/api/server/controllers/categories.js
+++ packages/custom/api/server/controllers/categories.js
@@ -9,7 +9,7 @@ var mongoose = require('mongoose'),
 exports.category = function (req, res, next, slug){
   Category.getDescendantsBySlug(slug, true, function (err, data){
     if (err){
-      res.status(500).json({
+      return res.status(500).json({
         error: 'Faild to load categories'
       });
     }
@@ -20,10 +20,43 @@ exports.category = function (req, res, next, slug){
 exports.all = function (req, res, next){
   Category.getAllRoot(function(err, data){
     if (err){
-      res.status(500).json({
+      return res.status(500).json({
         error: 'Faild to load categories'
       });
     }
     res.json(data);
+  });
+};
+
+exports.create = function(req, res, next){
+  var category = new Category(req.body);
+
+  req.assert('name', 'You must enter a name').notEmpty();
+  req.assert('slug', 'You must enter a slug').notEmpty();
+
+  var errors = req.validationErrors();
+  if (errors) {
+    return res.status(400).send(errors);
+  }
+
+  category.save(function(err){
+    if (err) {
+      var modelErrors = [];
+
+      if (err.errors) {
+
+        for (var x in err.errors) {
+          modelErrors.push({
+            param: x,
+            msg: err.errors[x].message,
+            value: err.errors[x].value
+          });
+        }
+
+        return res.status(400).json(modelErrors);
+      }
+    }
+
+    res.json(category);
   });
 };
--- packages/custom/api/server/routes/api.js
+++ packages/custom/api/server/routes/api.js
@@ -6,7 +6,10 @@ var categories = require('../controllers/categories');
 // The Package is past automatically as first parameter
 module.exports = function(Api, app, auth, database) {
 
-  app.route('/api/categories').get(categories.all);
+  app.route('/api/categories')
+    .get(categories.all)
+    .post(categories.create);
+
   app.route('/api/categories/:slug').get(categories.category);
 
   app.param('slug', categories.category);
